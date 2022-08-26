985c2cfdffc0901558fcbe0ebd91ac8ccddc8f04
Vi0214 <103825306+Vi0214@users.noreply.github.com>
2022-08-26 13:59:14 +0530

    Update gitrepository.groovy

--- vars/gitrepository.groovy
+++ vars/gitrepository.groovy
@@ -1,3 +1,3 @@
 def call(){
-  checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Vi0214/javawebapp.git']]])
+  checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Vi0214/sample-java-app.git']]])
 }
