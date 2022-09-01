afe66d526c6167111c232adb2c2fe6e15ab0af97
Vi0214 <103825306+Vi0214@users.noreply.github.com>
2022-09-01 11:26:37 +0530

    Update Deploy.groovy

--- vars/Deploy.groovy
+++ vars/Deploy.groovy
@@ -1,6 +1,5 @@
 def call {
-	      withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'Kube_config', namespace: '', serverUrl: '') {
-        sh "sed -i s/_BUILD_NUMBER_/${env.BUILD_NUMBER}/g eks-deploy-k8s.yaml"
+	withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'Kube_config', namespace: '', serverUrl: '') {
         sh "kubectl apply -f eks-deploy-k8s.yaml"
     }
 }
