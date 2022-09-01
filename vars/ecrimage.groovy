cf7886001b3516ffb2c6496436a867e681df9a0b
Vi0214 <103825306+Vi0214@users.noreply.github.com>
2022-09-01 11:23:24 +0530

    Update ecrimage.groovy

--- vars/ecrimage.groovy
+++ vars/ecrimage.groovy
@@ -1,4 +1,7 @@
 def call() {
 	echo "building and pushing an image to ECR....."
 	sh "docker build -t ${Image_name} ."
+	sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 975513479057.dkr.ecr.us-east-1.amazonaws.com"
+        sh "docker tag ${Image_name}:latest 975513479057.dkr.ecr.us-east-1.amazonaws.com/${Image_name}:latest"
+	sh "docker push 975513479057.dkr.ecr.us-east-1.amazonaws.com/${Image_name}:latest"
 }
